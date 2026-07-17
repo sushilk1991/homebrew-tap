e1f6108b9fca7a521809d3a95a936805aaad6753
Sushil Kumar <sushilk.1991@gmail.com>
2026-07-17 15:37:38 +0530

    velora 0.8.1

--- Casks/velora.rb
+++ Casks/velora.rb
@@ -1,6 +1,6 @@
 cask "velora" do
-  version "0.8.0"
-  sha256 "90276710de27d2d1f60b1244f03b4ff15462826ff5a6a7c142996508be44e14b"
+  version "0.8.1"
+  sha256 "921dc823ed4706dbe742702a8244ae8f94d090a708acadb74ef48c17c78da471"
 
   url "https://github.com/sushilk1991/velora/releases/download/v#{version}/Velora-#{version}.dmg"
   name "Velora"
